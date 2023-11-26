211bd7003ec5e8b77a5ef9e6d1fa287c8d7db53b
YokuraShio <11559407+rationnel@user.noreply.gitee.com>
2023-11-26 06:10:58 +0000

    Site updated: 2023-11-26 06:10:57

--- js/Lete.js
+++ js/Lete.js
@@ -1,1 +1,8 @@
-$(document).ready((function(t){$(".copyright").html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Lete乐特')}));var app=document.getElementById("footer");app.removeAttribute("id");
+// 设置页脚博主会动的心
+$(document).ready(function(e){
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Lete乐特');
+})
+// 移除页脚样式
+var app = document.getElementById('footer');
+app.removeAttribute('id')
+
